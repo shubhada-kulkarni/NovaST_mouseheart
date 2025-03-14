f2667a281e274e144ae357917d0678e9cb782222
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2025-03-14 15:59:48 +0100

    bash script to submit tile numbers python script

---
+++ run_slurm_tilenumbers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# shell script to submit python script get_tile_information_from_fasta.py as a SLURM jobs
+#
+#
+date
+
+echo "Job started"
+python get_tile_information_from_fasta.py /prj/NovaST/Dieterich_AAGMTVHM5/data/AAGMTVHM5_NovaST_25s000253-1-1_Dieterich_lane1sample1_1_sequence.txt.gz
+echo "Job ended!"
+
+date
